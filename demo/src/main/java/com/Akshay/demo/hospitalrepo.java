19c4d3f43394a460bc444ee43fa89b3d6a25ea7a
akshay-narwade <akshaynarwade5000@gmail.com>
2024-05-14 15:37:34 +0530

    first commit

---
+++ demo/src/main/java/com/Akshay/demo/hospitalrepo.java
@@ -0,0 +1,10 @@
+package com.Akshay.demo;
+
+
+ 
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface hospitalrepo extends JpaRepository<hospitaldetails,String>{
+   
+}
+   
---
+++ demo/src/main/java/com/Akshay/demo/impliments.java
@@ -0,0 +1,5 @@
+package com.Akshay.demo;
+
+public class impliments {
+
+}
